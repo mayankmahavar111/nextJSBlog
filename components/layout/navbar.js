2cd8fbc393c767396b025a6b69bb48538e470850
mayank <mayankmahavar111@gmail.com>
2022-02-20 14:45:32 +0530

    main done

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -23,8 +23,8 @@ export default function Navbar(props) {
             <div className={Styles.navBarInnerDiv}>
                 {navProps && navProps.length > 0 && navProps.map((item,keys)=>{
                     return<Link href={item.url} key={keys}> 
-                            <a className={Styles.link}> 
-                                <div  className={Styles.linkDiv}>
+                            <a className={Styles.link} style={{borderLeft: keys == 0 ? '1px black double': ''}}>  
+                                <div  className={Styles.linkDiv} >
                                     {item.name}
                                 </div>
                             </a>
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div>
       <main>
         <h1>
           My portfolio
@@ -12,6 +12,5 @@ export default function Home() {
           Hi, i am creating this website for testing and fun. Don`t mind my naiveness while designing the web page
         </div>
       </main>
-    </div>
   )
 }
