6093e0af5ea8afcaa635c50d1073b81e48a0b415
mayank <mayankmahavar111@gmail.com>
2022-02-20 13:46:51 +0530

    optimising nav bar

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,10 +1,20 @@
 import Link from 'next/link'
+import { useEffect , useState } from 'react';
 import Styles from '../../styles/navBar.module.css';
 
 export default function Navbar(props) {
     const navProps =  props.navItems ? props.navItems : [];
+    const [gridPercent, setGridPercent] =  useState('60% 40%');
+
+    useEffect(()=>{
+        if (typeof window !='undefined' && window.innerWidth < 800 && gridPercent == '60% 40%'){
+            setGridPercent('50% 50%')
+        }
+
+    },[])
+
     return (
-        <div className={Styles.navBarOuterDiv}>
+        <div className={Styles.navBarOuterDiv} style={{gridTemplateColumns: gridPercent}}>
             <div>
                 <Link href={'/'}>
                     <a className={Styles.logoLink}>M<sup>2</sup></a>
