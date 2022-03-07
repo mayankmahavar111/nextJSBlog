1fa927d027b94b2a5349a4d6fa6944187db52d72
mayank <mayankmahavar111@gmail.com>
2022-03-08 03:33:48 +0530

    link revamp and icons additon

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,57 @@
 
-export default function Contact() {
+import Link from "next/link"
+import { iconSvg } from "../components/api/helper"
+import Styles from '../styles/contact.module.css'
+
+export default function Contact(props) {
+
+    const contactList = [
+      {
+        'name' : 'Linkedin',
+        'link' : 'https://www.linkedin.com/in/mayank-mahavar/'
+      },
+      {
+        'name' : 'Github',
+        'link' : 'https://github.com/mayankmahavar111'
+      },
+      {
+        'name' : 'Gmail',
+        'link' : 'mailto:mayankmahavar111@gmail.com'
+      },  
+      {
+        'name' : 'Meta',
+        'link' : 'https://www.facebook.com/people/Mayank-Mahavar/100004140257021/'
+      },
+      {
+        'name' : 'Instagram',
+        'link' : 'https://www.instagram.com/mayankmahavar/'
+      },
+      {
+        'name' : 'Telegram',
+        'link' : 'https://t.me/mayankmahavar'
+      },
+      {
+        'name' : 'Whatsapp',
+        'link' : 'https://wa.me/+919643523146'
+      },
+    ]
+
     return (
           <main>
             <h1>
               Contact Info
             </h1>
             <div>
-              Hi, i am creating this website for testing and fun. Don`t mind my naiveness while designing the web page
+              Contact Form coming soon ... 
+            </div>
+            <div className={`displayFlex ${Styles.linkOuterDiv}`}> 
+              {contactList.map((item,key) => {
+                return <div className={`mAuto ${Styles.linkDiv}`} key={key}>
+                      <Link href={item.link}>
+                        <a>{iconSvg(item.name.toLocaleLowerCase())} {props.mobileView == false? item.name : ''}</a>
+                      </Link>
+                  </div>
+              })}
             </div>
           </main>
       )
--- pages/work.js
+++ pages/work.js
@@ -1,29 +1,16 @@
-
-import styles from '../styles/projects.module.css';
 import { useState } from 'react';
-import ProjectCard from '../components/layout/projectCard';
 
-function Projects(props) {
-  
-  const [projectList, setprojectList] = useState(props.projectList ? props.projectList.slice(0,5) : {});
-    return (
+function Work(props) {
+
+  return (
           <main>
             <h1>
               Work
             </h1>
-            <ProjectCard 
-              projectList= {projectList}
-            />
+            <div>
+              Work section is coming soon ... 
+            </div>
           </main>
       )
 }
-
-Projects.getInitialProps = async (context) =>{
-  const res  = await fetch("https://api.github.com/users/mayankmahavar111/repos")
-  const jsonRes = await res.json();
-  return {
-    projectList : jsonRes
-  };
-}
-
-export default Projects;
+export default Work;
