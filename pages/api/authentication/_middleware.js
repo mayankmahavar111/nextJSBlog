20884081c5e3531daaae7fac82152bf5eb5b602c
mayank <mayankmahavar111@gmail.com>
2022-03-23 04:52:18 +0530

    middleware bug commented for now

--- pages/api/authentication/_middleware.js
+++ pages/api/authentication/_middleware.js
@@ -1,27 +1,27 @@
 import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
-import { sessionOptions } from '../../../components/api/session'
-import { withIronSessionApiRoute } from 'iron-session/next'
+// import { sessionOptions } from '../../../components/api/session'
+// import { withIronSessionApiRoute } from 'iron-session/next'
 
-export default withIronSessionApiRoute(middleware , sessionOptions);
+// export default withIronSessionApiRoute(middleware , sessionOptions);
 
-async function middleware(req= NextRequest, ev= NextFetchEvent, res =NextResponse) {
+export default async function middleware(req= NextRequest, ev= NextFetchEvent, res =NextResponse) {
     const url =  req.nextUrl.clone()
     url.pathname= '/login';
 
     if(req.method ==='GET'){
       res.redirect(307,url)
     }
-    const user =await req.session.user;
-    console.log("inside middle warre" , user);
-    if (user && user.isLoggedIn){
-      return NextResponse.status(200).send({
-        ...user,
-        ...{
-          message : "Already logged in",
-          statusCode : 200
-        }
-      })
-    }
+    // const user =await req.session.user;
+    // console.log("inside middle warre" , user);
+    // if (user && user.isLoggedIn){
+    //   return NextResponse.status(200).send({
+    //     ...user,
+    //     ...{
+    //       message : "Already logged in",
+    //       statusCode : 200
+    //     }
+    //   })
+    // }
 
     return NextResponse.next();
   }
