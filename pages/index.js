eda036961438c0d0c71972d8c7fa2056c9a58bcb
mayank <mayankmahavar111@gmail.com>
2022-03-28 14:51:43 +0530

    adding resume to home page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
-import styles from '../styles/Home.module.css'
-import { useEffect, useState } from 'react';
-import Link from 'next/link'
 
 export default function Home(props) {
+
+  const resumeUrl = 'https://drive.google.com/file/d/1wF9U64vdH7RJSHkU7mhnu4B5XJSKlBEw/view?usp=sharing';
+
+  const downloadResume = ()=>{
+    if(window){
+      window.open(resumeUrl,'pdf')
+    }
+  }
+
   return (
       <main>
         <h1>
@@ -10,6 +16,9 @@ export default function Home(props) {
         </h1>
         <div>
           Hi, i am creating this website for testing and fun. Don`t mind my naiveness while designing the web page.
+        </div>
+        <div>
+          <button onClick={downloadResume}>Download Resume</button>
         </div>
       </main>
   )
